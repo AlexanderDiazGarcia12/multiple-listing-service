59bf75df60017823fd023eb3bbcbc94e39749875
Alexander Diaz Garcia <alexander.diazg@outlook.com>
2024-06-23 23:07:35 -0500

    Creation of the MLS spring project

---
+++ src/main/java/com/adg/realestate/multiple_listing_service/MultipleListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adg.realestate.multiple_listing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleListingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adg/realestate/multiple_listing_service/MultipleListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adg.realestate.multiple_listing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
